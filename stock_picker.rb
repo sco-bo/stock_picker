e8b03db95321fbaf70be62791572087cd5a53c66
sco-bo <scottebobbitt@gmail.com>
2015-08-18 14:21:48 -0400

    add puts message

--- stock_picker.rb
+++ stock_picker.rb
@@ -1,30 +1,24 @@
-def stock_prices (array)
+def stock_prices array
 	$largest_difference = 0
 	array.each_with_index {|value, index|
-		if index == array.size - 1
-			exit
-		end
-		puts "Value is #{value}"
 		array.each {|i| 
 			$difference = value -  i
-			puts "The index of this loop is " + array.index(i).to_s
-			puts "Difference is #{$difference}"
-			puts "'index' is #{index}"
 			if ($difference <= $largest_difference) && (index < array.rindex(i))
 				$negative_array = [] << $difference
 				$negatives = [index, array.rindex(i)]
-				puts "Values added to negatives array"
 				$largest_difference = $difference
 			end
-			p $negative_array
-			puts "Negatives is #{$negatives}"
-			}	
+		}	
 	}
+	if $negative_array.nil?
+		puts "The stock should be bought and sold at [0, 1], respectively"
+	else
+		puts "The stock should be bought and sold at #{$negatives}, respectively"
+	end
 end
 
-
 stock_prices([17,3,6,9,15,8,6,1,10])
-stock_prices([25,2,10,9])
-stock_prices([10,12,5,3,20,1,9,20])
-stock_prices([10,9,8,7,6])
-stock_prices([17,17,16,15])
+#stock_prices([25,2,10,9])
+#stock_prices([10,12,5,3,20,1,9,20])
+#stock_prices([10,9,8,7,6])
+#stock_prices([18,17,17,16,15])
